1abfd8f1fd9acc4f7134f19c4b34858c8e7b636e
harshad bankar <harshad.bankar@klm.com>
2017-05-11 11:38:57 +0200

    Angular Component & Data binding  - Till lecture 31

---
+++ regular-course-code/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',//element selector
+  // selector: '[app-servers]',//attribute selector
+  // selector:'.app-servers',// css selector
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! and name is '+this.serverName;
+  }
+  
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value; // Need explicit casting here, to inform event which we are receiving is of type html element
+
+  }
+}
